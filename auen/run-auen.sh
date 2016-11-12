cbebef64db941d42c263f7dab857f565ba4ae1ff
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-11-12 16:48:35 -0600

    Try CANDLE Python

--- auen/run-auen.sh
+++ auen/run-auen.sh
@@ -6,7 +6,15 @@ set -eu
 
 echo run-auen $*
 
-PYTHON=/usr/bin/python
-unset LD_LIBRARY_PATH PYTHONPATH
+# PYTHON=/usr/bin/python
+PYTHON=/lustre/beagle2/lpBuild/CANDLE/python/Python-2.7.12-inst/bin/python
+
+export LD_LIBRARY_PATH=/lustre/beagle2/lpBuild/CANDLE/python/Python-2.7.12-inst/lib
+
+# unset LD_LIBRARY_PATH PYTHONPATH
+
+export HOME=/lustre/beagle2/$USER
+declare HOME
+
 
 $PYTHON ./run-auen.py $*
