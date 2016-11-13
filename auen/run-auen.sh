23d94ddb7034c1a4ef9d1c3bb570297032ef0e85
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-11-12 20:38:20 -0600

    Adding run-auen.sh

--- auen/run-auen.sh
+++ auen/run-auen.sh
@@ -2,30 +2,21 @@
 set -eu
 
 # RUN-AUEN
-# Shell wrapper for AUEN to set up PATH, PYTHONPATH, etc.
+# Shell wrapper for AUEN to set up PATH, PYTHONPATH, LD_LIBRARY_PATH, etc.
 
-set -x
+echo RUN-AUEN $*
 
 echo PWD $PWD
-cd $PBS_O_WORKDIR
-echo PWD $PWD
-echo run-auen $*
 
 # PYTHON=/usr/bin/python
 PYTHON=/lustre/beagle2/lpBuild/CANDLE/python/Python-2.7.12-inst/bin/python
 
-export LD_LIBRARY_PATH=/lustre/beagle2/lpBuild/CANDLE/python/Python-2.7.12-inst/lib
-
-# unset LD_LIBRARY_PATH PYTHONPATH
-
+# Override HOME to use the Keras rc file in /lustre
 export HOME=/lustre/beagle2/$USER
-echo HOME $HOME
 
-# printenv
+# source /opt/modules/3.2.6.7/init/bash
+# module load PrgEnv-gnu
 
-source /opt/modules/3.2.6.7/init/bash
-module load PrgEnv-gnu
+export LD_LIBRARY_PATH=/opt/gcc/4.9.2/snos/lib64:/lustre/beagle2/lpBuild/CANDLE/python/Python-2.7.12-inst/lib
 
-aprun -n 1 $PYTHON /lustre/beagle2/wozniak/proj/codar/auen/run-auen.py ARGS()
-# aprun -n 1 $PYTHON /lustre/beagle2/wozniak/proj/codar/auen/try_pandas.py $*
-# aprun $PYTHON -V
+$PYTHON /lustre/beagle2/wozniak/proj/codar/auen/run-auen.py $*
