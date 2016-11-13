28a90f33cfccf14f5c1d3f5a29c130ac239f88e3
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-11-12 21:20:27 -0600

    Adding run-swift.sh

---
+++ auen/run-swift.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eu
+
+export TURBINE_OUTPUT_ROOT=$PWD
+export TURBINE_OUTPUT_FORMAT=out-%Q
+
+PATH=/lustre/beagle2/wozniak/Public/sfw/swift-t/py2Lr/stc/bin:$PATH
+
+PROCS=3
+swift-t -m cray -n $PROCS run-many.swift --auen_home=$PWD
