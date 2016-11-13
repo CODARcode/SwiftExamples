3724061e8d1ccebaa6e7ab4a6bee3f315de81da7
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-11-12 21:13:12 -0600

    Support auen_home

--- auen/run-auen.py
+++ auen/run-auen.py
@@ -9,12 +9,16 @@ import sys
 
 import auen_ff
 
-if len(sys.argv) != 4:
+if len(sys.argv) == 1:
     print("Requires three arguments: <NI> <NE> <OUTPUT>")
     sys.exit(1)
 
+auen_home = "."
+    
 N1 = float(sys.argv[1])
 NE = float(sys.argv[2])
 output = sys.argv[3]
+if len(sys.argv) == 5:
+    auen_home = sys.argv[4]
     
-auen_ff.run_one(N1, NE, output)
+auen_ff.run_one(N1, NE, output, auen_home)
