7a4fc9fe28e003f054d2f83128c973b1a9c99d2c
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-11-13 12:19:48 -0600

    Minor fixes

--- auen/qtests/run-auen-submit.sh
+++ auen/qtests/run-auen-submit.sh
@@ -4,20 +4,16 @@ set -eu
 # RUN-AUEN
 # Shell wrapper for AUEN to set up PATH, PYTHONPATH, etc.
 
-set -x
-
 echo PWD $PWD
 cd $PBS_O_WORKDIR
 echo PWD $PWD
-echo run-auen $*
 
 # PYTHON=/usr/bin/python
 PYTHON=/lustre/beagle2/lpBuild/CANDLE/python/Python-2.7.12-inst/bin/python
 
 export LD_LIBRARY_PATH=/lustre/beagle2/lpBuild/CANDLE/python/Python-2.7.12-inst/lib
 
-# unset LD_LIBRARY_PATH PYTHONPATH
-
+# Override HOME to use the Keras rc file in /lustre
 export HOME=/lustre/beagle2/$USER
 echo HOME $HOME
 
@@ -26,6 +22,4 @@ echo HOME $HOME
 source /opt/modules/3.2.6.7/init/bash
 module load PrgEnv-gnu
 
-aprun -n 1 $PYTHON /lustre/beagle2/wozniak/proj/codar/auen/run-auen.py ARGS()
-# aprun -n 1 $PYTHON /lustre/beagle2/wozniak/proj/codar/auen/try_pandas.py $*
-# aprun $PYTHON -V
+aprun -n 1 $PYTHON /lustre/beagle2/wozniak/proj/codar/auen/run-auen.py 500 500 stdout $PWD
