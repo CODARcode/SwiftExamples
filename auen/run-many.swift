5948393fd7e4fe87925c45caae175dd1726d11f4
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-11-12 21:13:40 -0600

    Support auen_home

---
+++ auen/run-many.swift
@@ -0,0 +1,23 @@
+
+// RUN-MANY
+
+// Run with:
+// $ swift-t run-many.swift
+
+import sys;
+
+string auen_home = argv("auen_home");
+string auen = auen_home+"/run-auen.sh";
+
+app auen_ff(int N1, int NE, string auen_home)
+{
+  auen N1 NE "stdout" auen_home;
+}
+
+// Fixed value for now
+NE = 600;
+
+foreach N1 in [100,500,1000,1500,2000,2500,3000]
+{
+  auen_ff(N1, NE, auen_home);
+}
