3ce7d164b1e26ec565499a88c1d49374dad71903
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-11-14 14:14:13 -0600

    Set LD_LIBRARY_PATH

--- auen/run-swift.sh
+++ auen/run-swift.sh
@@ -7,5 +7,7 @@ export WALLTIME=01:00:00
 
 PATH=/lustre/beagle2/wozniak/Public/sfw/swift-t/py2Lr/stc/bin:$PATH
 
+export LD_LIBRARY_PATH=/opt/gcc/4.9.2/snos/lib64:/lustre/beagle2/lpBuild/CANDLE/python/Python-2.7.12-inst/lib
+
 PROCS=3
 swift-t -m cray -n $PROCS run-many.swift --auen_home=$PWD
