c70cef09189ce3a369cd443503c0c2a666f3cbca
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-11-29 18:30:44 -0600

    This will fail more obviously

--- oversample/oversample-workflow.sh
+++ oversample/oversample-workflow.sh
@@ -8,9 +8,7 @@ export TURBINE_OUTPUT_ROOT=$PWD
 export TURBINE_OUTPUT_FORMAT=out-%Q
 export WALLTIME=01:00:00
 
-export PATH=/lustre/beagle2/wozniak/Public/sfw/swift-t/py2Lr/stc/bin:$PATH
-
-which swift-t
+SWIFT=/lustre/beagle2/wozniak/Public/sfw/swift-t/py2Lr/stc/bin/swift-t
 
 PROCS=${PROCS:-2}
-swift-t -m cray -n 2 -t i:./pre.sh oversample.swift
+$SWIFT -m cray -n 2 -t i:./pre.sh oversample.swift
