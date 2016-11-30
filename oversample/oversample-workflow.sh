a8b8708d48b628e4d393ac7548ba5fe6208f522f
ianfoster <foster@uchicago.edu>
2016-11-29 18:00:02 -0600

    my change

--- oversample/oversample-workflow.sh
+++ oversample/oversample-workflow.sh
@@ -8,7 +8,10 @@ export TURBINE_OUTPUT_ROOT=$PWD
 export TURBINE_OUTPUT_FORMAT=out-%Q
 export WALLTIME=01:00:0
 
-export PATH=/lustre/beagle2/wozniak/Public/sfw/swift-t/py2Lr/stc/bin:$PATH
+export PATH=/lustre/beagle1/wozniak/Public/sfw/swift-t/py2Lr/stc/bin:$PATH
 
-swift-t -m cray -t i:./pre.sh oversample.swift
+export HOME=/lustre/beagle2/foster
+
+PROCS=2
+swift-t -m cray -n $PROCS -t i:./pre.sh oversample.swift
 
