5f2d31803ddb70816d316d3c52fdd788ac652ac4
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-12-15 20:44:58 -0600

    Progress- TensorFlow seems to work, now stuck on pandas

--- auen/qtests/run-auen-submit.sh
+++ auen/qtests/run-auen-submit.sh
@@ -5,13 +5,20 @@ set -eu
 # PBS submit script for AUEN: sets up PATH, PYTHONPATH, etc.
 # Invoke submit-run.sh to launch this
 
+#PBS -j oe
+
 cd $PBS_O_WORKDIR
 echo PWD=$PWD
 
-# PYTHON=/usr/bin/python
-PYTHON=/lustre/beagle2/lpBuild/CANDLE/python/Python-2.7.12-inst/bin/python
+PYTHON_HOME=/lustre/beagle2/lpBuild/CANDLE/python/Python-2.7.12-inst
 
-export LD_LIBRARY_PATH=/lustre/beagle2/lpBuild/CANDLE/python/Python-2.7.12-inst/lib:/opt/gcc/4.9.2/snos/lib64
+# PYTHON=/usr/bin/python
+PYTHON=$PYTHON_HOME/bin/python
+
+TF=$PYTHON_HOME/lib/python2.7/site-packages/tensorflow/python
+
+export LD_LIBRARY_PATH=$PYTHON_HOME/lib:/opt/gcc/4.9.2/snos/lib64:$TF
+export PYTHONPATH=$TF
 
 # Override HOME to use the Keras rc file in /lustre
 export HOME=/lustre/beagle2/$USER
@@ -23,4 +30,4 @@ echo HOME=$HOME
 # source /opt/modules/3.2.6.7/init/bash
 module load PrgEnv-gnu
 
-aprun -n 1 $PYTHON /lustre/beagle2/wozniak/proj/codar/auen/run-auen.py 500 500 stdout $PWD
+aprun -n 1 $PYTHON $PWD/../run-auen.py 500 500 stdout $PWD
