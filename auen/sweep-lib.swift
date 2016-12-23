188f0a5fe3196cd9fdc7a37436b6a848fbbc7700
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-12-23 14:04:36 -0600

    New versions to call AUEN through library interface

---
+++ auen/sweep-lib.swift
@@ -0,0 +1,29 @@
+
+// SWEEP-LIB.SWIFT
+
+// Run with:
+// $ swift-t sweep-lib.swift --auen_home=$PWD
+
+import io;
+import python;
+import sys;
+
+printf("SWEEP-LIB.SWIFT");
+
+// string auen_home = argv("auen_home");
+// string auen = auen_home+"/run-auen.sh";
+
+// auen_ff(int N1, int NE, string auen_home)
+// {
+//   auen N1 NE "stdout" auen_home;
+// }
+
+// // Fixed value for now
+// NE = 600;
+
+// foreach N1 in [100,500,1000,1500,2000,2500,3000]
+// {
+//   auen_ff(N1, NE, auen_home);
+// }
+
+printf(python("import auen_ff", "repr(0)"));
