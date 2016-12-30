9d0059f07035369dbce21ca33eb2cc50b88c496c
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-12-30 12:49:36 -0600

    Support NE argument to sweep-lib.swift

--- auen/sweep-lib.swift
+++ auen/sweep-lib.swift
@@ -2,7 +2,7 @@
 // SWEEP-LIB.SWIFT
 
 // Run with:
-// $ swift-t sweep-lib.swift --auen_home=$PWD
+// $ swift-t sweep-lib.swift --auen_home=$PWD [--NE=<NE>]
 
 import io;
 import python;
@@ -10,15 +10,17 @@ import sys;
 
 printf("SWEEP-LIB.SWIFT");
 
-string auen_home = argv("auen_home");
+auen_home = argv("auen_home");
 
-foreach N1 in [100,500,1000,1500,2000,2500,3000] 
+NE = parseInt(argv("NE", "600"));
+
+foreach N1 in [100,500,1000,1500,2000,2500,3000]
 {
-printf("Python result: %s", 
+printf("Python result: %s",
 python_persist(
 "import auen_ff",
 """
-auen_ff.run_one(%i, 600, "%s")
+auen_ff.run_one(%i, %i, "%s")
 """
-% (N1, auen_home)));
+% (N1, NE, auen_home)));
 }
