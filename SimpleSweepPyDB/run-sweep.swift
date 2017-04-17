889e24441fbca3e44df5f6d5263854dc29d02669
Justin M Wozniak <wozniak@mcs.anl.gov>
2017-04-17 12:39:05 -0500

    Clean

--- SimpleSweepPyDB/run-sweep.swift
+++ SimpleSweepPyDB/run-sweep.swift
@@ -11,11 +11,11 @@ settingsFilename = argv("settings");
 // Settings file should have form:
 //   { 'parameters' : {'1' : numList1, '2': numList2, ...} }
 // where each numList is a list of integers or floats.
-//
+
 // determineParameters then returns a string with the cross-product of all combinations of numbers from each list.
 // E.g., if there are three parameters:
 //    'n1a,n2a,n3a:n1a,n2b,n3a:...'
-//
+
 parametersString = python("import sweep", sprintf("sweep.determineParameters(\"%s\")", settingsFilename));
 // We split that string to get a list of comma-separated parameter combinations
 parameters = split(parametersString, ":");
