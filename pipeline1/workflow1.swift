1e050849c7acf836142ca10b39257b027dd14b6f
Justin Wozniak <wozniak@mcs.anl.gov>
2017-06-14 21:17:47 -0500

    Make it a 3-app pipeline

--- pipeline1/workflow1.swift
+++ pipeline1/workflow1.swift
@@ -7,8 +7,11 @@ import string;
 
 a_x = "apps/a.x";
 b_x = "apps/b.x";
+c_x = "apps/c.x";
 
-int procs[] = [4, 2];
-string cmd[] = [ a_x, b_x ];
-string argv[] = [ "f1.data f2.data", "f2.data f3.data" ];
+int procs[] = [4, 2, 3];
+string cmd[] = [ a_x, b_x, c_x ];
+string argv[] = [ "f1.data f2.data",
+                  "f2.data f3.data",
+                  "f3.data f4.data" ];
 @par=sum_integer(procs) launch_multi(procs, cmd, argv);
