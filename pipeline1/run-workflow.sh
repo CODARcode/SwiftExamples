559c3b59cd1385d8a621d76186ff0496a37d5f78
Justin Wozniak <wozniak@mcs.anl.gov>
2017-06-20 13:48:07 -0500

    Add some notes and positive feedback

--- pipeline1/run-workflow.sh
+++ pipeline1/run-workflow.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -eu
 
+# RUN WORKFLOW
+# Runs the demo workflow1.swift
+
 if [ ${#} != 1 ]
 then
   echo "Requires PROCS!"
@@ -16,5 +19,12 @@ MACHINE=${MACHINE:-}
 
 THIS=$( dirname $0 )
 
+# This does not work with OpenMPI, but
+# if trying to fix this for OpenMPI, you may need this:
+# (cf. https://groups.google.com/forum/#!topic/maker-devel/RnXzOWDSFk0)
+# export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libmpi.so.20
+
 stc -p -u -I $LAUNCH -r $LAUNCH workflow1.swift
 turbine -n $PROCS $MACHINE workflow1.tic
+
+echo "SUCCESS!"
