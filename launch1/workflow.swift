0c14604a87fc03abfee741732ef0e082df153af4
Justin Wozniak <wozniak@mcs.anl.gov>
2017-07-20 14:59:54 -0500

    Use swift:launcher on Titan

--- launch1/workflow.swift
+++ launch1/workflow.swift
@@ -9,9 +9,10 @@ import launch;
 program = "../apps/noop.x";
 
 string arguments[] = [ "-v", "-d", "3" ];
+string envs[] = [ "swift:launcher=/xyz" ];
 p = 2;
 printf("swift: launching: %s on %i processes", program, p);
-exit_code = @par=p launch(program, arguments);
+exit_code = @par=p launch_envs(program, arguments, envs);
 printf("swift: received exit code: %d", exit_code);
 if (exit_code != EXIT_SUCCESS)
 {
